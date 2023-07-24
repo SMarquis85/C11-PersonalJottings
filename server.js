8f3f51ffcde13a95403c35f6b4dbfb3fde554561
SMarquis85 <sophienathalie_85@outlook.com>
2023-07-24 19:29:28 -0400

    updated server.js file

--- server.js
+++ server.js
@@ -6,16 +6,12 @@ const path = require('path');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
-app.use(express.urlencoded({
-    extended: true
-}));
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(express.json());
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-
 app.listen(PORT, () => {
-    console.log('API server now on port ${PORT}!');
+  console.log(`API server now on port ${PORT}!`);
 });
